a9f3ae810555a37a92dcc1d98e43901e0f25e9ba
chenxizhang <ares@xizhang.com>
2023-12-30 13:51:03 +0800

    add clarify js

--- engine.js
+++ engine.js
@@ -6,8 +6,17 @@ const script = `
   gtag('js', new Date());
 
   gtag('config', 'G-6MN194TCGY');
+
+    (function (c, l, a, r, i, t, y) {
+        c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "kdv3vgopva");
 </script>
 `
+
+
+
 
 module.exports = ({ marp }) => marp.use((md) => {
     const { normalizeLink } = md
